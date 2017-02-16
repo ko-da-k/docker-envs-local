5c0f304419078621a4ad436f5a31c025a95b016f
kKATO_lab <di.aps53@gmail.com>
2017-02-16 16:00:09 +0900

    leaflet marker sample is implemented

--- osm/app/static/marker_sample.js
+++ osm/app/static/marker_sample.js
@@ -1,19 +1,21 @@
 //leaflet OSM map
+// create a map in the "map_elemnt" div,
+// set the view to a given place and zoom
+var map = L.map('map_elemnt');
+map.setView([35.40, 139.50], 11);
+
+// add an OpenStreetMap tile layer
+var tileLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
 window.onload = function () {
-
-    // create a map in the "map_elemnt" div,
-    // set the view to a given place and zoom
-    var map = L.map('map_elemnt');
-    map.setView([35.40, 139.50], 11);
-
-    // add an OpenStreetMap tile layer
-    var tileLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-
     // add a marker in the given location,
     // attach some popup content to it and open the popup
-    var mapMarker = L.marker([35.40, 139.50])
+    var mapMarker1 = L.marker([35.40, 139.50])
+                     .addTo(map)
+                     .bindPopup('CSS3 popup. <br> ここはどこでしょうか？');
+    var mapMarker2 = L.marker([35.50, 140.00])
                      .addTo(map)
                      .bindPopup('CSS3 popup. <br> ここはどこでしょうか？');
 
@@ -22,11 +24,27 @@ window.onload = function () {
         "OpenStreetMap": tileLayer
     };
     var overlays = {
-        "Marker": mapMarker,
+        "Marker1": mapMarker1
     };
     L.control.layers(baseLayers, overlays).addTo(map);
 
     // add control scale 
     L.control.scale().addTo(map);
 
+    $.getJSON("/static/data/geodata.json", function(data) {
+        var geojson = L.geoJson(data, {
+            onEachFeature: onEachFeature
+        });
+        geojson.addTo(map);
+    });
+}
+
+map.on("click", function(e) {
+    console.log("Clicked Point:" + e.latlng.lng + " " + e.latlng.lat);
+});
+
+function onEachFeature(feature, layer) {
+    if (feature.properties && feature.properties.popupContent) {
+        layer.bindPopup(feature.properties.popupContent)
+    }
 }
